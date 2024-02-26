b19fefbfa97da5b7a64d0c1ccfae65709f61321f
frank gimeno <frankgimeno3@gmail.com>
2024-02-26 13:40:10 +0100

    añado calendario table calendar

--- lib/features/user_auth/presentation/pages/calendar.dart
+++ lib/features/user_auth/presentation/pages/calendar.dart
@@ -1,70 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import '../../../../global/common/toast.dart';
 
-
 class CalendarPage extends StatelessWidget {
   const CalendarPage({Key? key}) : super(key: key);
+
+  DateTime get today => DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Task List"),
+        title: const Text("Calendar"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GestureDetector(
-              onTap: () {
-                showToast(message: "This should open the calendar");
-              },
-              child: Container(
-                margin: const EdgeInsets.symmetric(vertical: 3),
-                height: 60,
-                width: 125,
-                decoration: BoxDecoration(
-                    color: Colors.purple,
-                    borderRadius: BorderRadius.circular(10)),
-                child: const Center(
-                  child: Text(
-                    "Calendar",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18),
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, "/home");
-              },
-              child: Container(
-                height: 45,
-                width: 100,
-                decoration: BoxDecoration(
-                    color: Colors.purple,
-                    borderRadius: BorderRadius.circular(10)),
-                child: const Center(
-                  child: Text(
-                    "Go back",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18),
-                  ),
-                ),
-              ),
-            ),
+            const Text("124"),
+            TableCalendar(
+              focusedDay: today,
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(3050, 12, 31),
+            )
           ],
         ),
       ),
     );
   }
-
-
 }
