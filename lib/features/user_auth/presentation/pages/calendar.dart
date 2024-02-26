cb6e1bfeaff9fcf68a997e34733d691ed9435292
frank gimeno <frankgimeno3@gmail.com>
2024-02-26 15:17:24 +0100

    mejoras en calendario

--- lib/features/user_auth/presentation/pages/calendar.dart
+++ lib/features/user_auth/presentation/pages/calendar.dart
@@ -55,7 +55,7 @@ class _CalendarPageState extends State<CalendarPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Calendar"),
+        title: const Text("Calendario"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
